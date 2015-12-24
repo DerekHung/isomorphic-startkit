ef20c162d79b0c01817d149ba049ad00c05c5a5e
Derek Hung <derek.hung@fanzytv.com>
2015-12-24 15:43:18 +0800

    babel setting

--- webpack.config.js
+++ webpack.config.js
@@ -14,31 +14,25 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	module: {
-		loaders: [{
-			test: /\.js$/,
-			loader: 'babel',
-			exclude: /node_modules/,
-			include: __dirname,
-			query: {
-				optional: ['runtime'],
-				stage: 2,
-				env: {
-					development: {
-						plugins: [
-							'react-transform'
-						],
-						extra: {
-							'react-transform': {
-								transforms: [{
-									transform:  'react-transform-hmr',
-									imports: ['react'],
-									locals:  ['module']
-								}]
-							}
-						}
-					}
-				}
-			}
-		}]
+		loaders: [
+		{
+            test: /\.(js|jsx)$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            include: __dirname,
+            query: {
+            	optional: ['runtime'],
+                plugins: [
+                    ['react-transform', {
+                        'transforms': [{
+                            'transform': 'react-transform-hmr',
+                            'imports': ['react'],
+                            'locals': ['module']
+                        }]
+                    }]
+                ]
+            }
+        },
+		]
 	}
 };
