c5a7bc37fd8a02e883bee19b0b74380d8c5f9ba1
Derek Hung <derek.hung@fanzytv.com>
2015-12-24 16:06:24 +0800

    babel 6

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-require('babel/register');
+require('babel-core/register');
 var backendServer = require('./server.js');
 
 module.exports = function(frontendServer){
+	console.log(backendServer);
 	return backendServer(frontendServer);
 };
