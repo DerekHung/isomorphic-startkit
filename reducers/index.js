bf4818c958509d0fcc3c8f34ab131a71885f68d4
rexhome7326 <rexhome7326@gmail.com>
2015-12-24 13:54:10 +0800

    delete something components about userData

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import questions from 'reducers/questions';
-import userData from 'reducers/userData';
 
 const rootReducer = combineReducers({
-	questions,
-	userData
+	questions
 });
 
 export default rootReducer;
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ import { Router, Route, IndexRoute } from 'react-router';
 import App from 'containers/App';
 import Intro from 'containers/Intro';
 import Question from 'containers/Question';
-import UserDataPage from 'containers/UserDataPage';
-import UserUpdatePage from 'containers/UserUpdatePage';
 
 export default function(history) {
 	return (
@@ -13,8 +11,6 @@ export default function(history) {
 			<Route path="/" component={App}>
 				<IndexRoute component={Intro} />
 				<Route path="/question/:questionId" component={Question} />
-				<Route path="/user/:pid" component={UserDataPage} />
-				<Route path="/user/update/:pid" component={UserUpdatePage} />
 			</Route>
 		</Router>
 	);
